e2f4ba115b579e176c067247cb346e146adb9902
Lorenzo Berni <duplo@develer.com>
2016-04-22 19:45:22 +0200

    Use ini configuration file instead of constants

--- client/src/connection.py
+++ client/src/connection.py
@@ -1,15 +1,14 @@
 import socket
 
-# TODO: add this to some common settings file
-DEFAULT_SOCKET_TIMEOUT=0.1
-
 
 class Connection:
     """Connection management class."""
 
-    def __init__(self, host, port, timeout=DEFAULT_SOCKET_TIMEOUT):
-        self.socket = socket.create_connection((host, port))
-        self.socket.settimeout(0.1)
+    def __init__(self, config):
+        c = config['Network']
+        conf = (c['ServerIPAddress'], c.getint('ServerPort'))
+        self.socket = socket.create_connection(conf)
+        self.socket.settimeout(c.getfloat('SocketTimeout'))
 
     def read(self, size=1024):
         data = None
--- client/src/main.py
+++ client/src/main.py
@@ -1,46 +1,24 @@
 #!/usr/bin/env python
 
-from IPy import IP
 from client import Client
 from client import sdl2context
+from configparser import ConfigParser
 from connection import Connection
-import click
+import os
 
 
-class IpAddress(click.ParamType):
-    """Custom click type for ip addresses.
-    """
-    name = 'ip address'
-
-    def convert(self, value, param, ctx):
-        try:
-            ip_address = IP(value)
-        except ValueError:
-            msg = "{} is not a valid ip address".format(value)
-            self.fail(msg)
-        return str(ip_address)
-
-IP_ADDRESS = IpAddress()
-DEFAULT_SERVER_IP_ADDRESS = IP('127.0.0.1')
-DEFAULT_SERVER_PORT = 1234
+CONFIG_FILE = os.path.join(os.getcwd(), 'client.ini')
 
 
-@click.command()
-@click.argument(
-    'host',
-    required=True, type=IP_ADDRESS, default=DEFAULT_SERVER_IP_ADDRESS,
-    metavar='IP')
-@click.argument(
-    'port',
-    required=True, type=click.INT, default=DEFAULT_SERVER_PORT, metavar='PORT')
 @sdl2context()
-def main(host, port):
-    print('starting application with on {} {}'.format(host, port))
-    conn = Connection(host, port)
+def main(config):
+    conn = Connection(config)
     client = Client(1024, 768, conn)
     client.start()
     client.quit()
 
 
 if __name__ == '__main__':
-    main()
+    config = ConfigParser()
+    config.read(CONFIG_FILE)
+    main(config)
