f1a5ed1ac9cf59571e3a82855eef8537d1e81762
Lorenzo Berni <duplo@develer.com>
2016-04-22 16:07:55 +0200

    Merge main

--- client/src/main.py
+++ client/src/main.py
@@ -1,9 +1,40 @@
-from client import sdl2context
+#!/usr/bin/env python
+
+from IPy import IP
 from client import Client
+from client import sdl2context
+import click
 
 
+class IpAddress(click.ParamType):
+    """Custom click type for ip addresses.
+    """
+    name = 'ip address'
+
+    def convert(self, value, param, ctx):
+        try:
+            ip_address = IP(value)
+        except ValueError:
+            msg = "{} is not a valid ip address".format(value)
+            self.fail(msg)
+        return ip_address
+
+IP_ADDRESS = IpAddress()
+DEFAULT_SERVER_IP_ADDRESS = IP('127.0.0.1')
+DEFAULT_SERVER_PORT = 1234
+
+
+@click.command()
+@click.argument(
+    'server',
+    required=True, type=IP_ADDRESS, default=DEFAULT_SERVER_IP_ADDRESS,
+    metavar='IP')
+@click.argument(
+    'port',
+    required=True, type=click.INT, default=DEFAULT_SERVER_PORT, metavar='PORT')
 @sdl2context()
-def main():
+def main(server, port):
+    click.echo('{}:{}'.format(server, port))
     client = Client(1024, 768)
     client.start()
     client.quit()
