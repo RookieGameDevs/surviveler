1f1e76dde3653edb705d2601dca1ca871b86a471
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-04-24 21:07:56 +0200

    Cosmetics

--- server/src/server/core/messages.go
+++ server/src/server/core/messages.go
@@ -1,5 +1,5 @@
 /*
-	Common Message definitions
+	Message interfaces
 */
 package core
 
@@ -7,10 +7,17 @@ import (
 	"net"
 )
 
+// Message is the interface that wraps the Serialize method.
 type Message interface {
+
+	// Serialize transforms the message into a buffer of bytes.
 	Serialize() []byte
 }
 
+// MessageReader is the interface that wraps the ReadMessage method.
 type MessageReader interface {
+
+	// ReadMessage reads a message from a TCP connection, or returns an error
+	// in case of failure.
 	ReadMessage(conn *net.TCPConn) (Message, error)
 }
