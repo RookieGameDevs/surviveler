b43da35715d442328f1d3ff3efe5bdcb89836a81
Ivan Nikolaev <voidexp@gmail.com>
2016-04-26 23:33:25 +0200

    Style fix

--- client/src/exceptions.py
+++ client/src/exceptions.py
@@ -1,21 +1,17 @@
 class ConfigError(Exception):
     """Missing or invalid configuration entry."""
-    pass
 
 
 class SDLError(Exception):
     """Generic SDL error."""
-    pass
 
 
 class OpenGLError(Exception):
     """Generic OpenGL error."""
-    pass
 
 
 class ShaderError(Exception):
     """Shader compile/link error."""
-    pass
 
 
 class UniformError(Exception):
