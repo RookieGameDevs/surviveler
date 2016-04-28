2fbe805a5530700b132a6525b08b4f03f9240a5b
Iacopo Marmorini <iacopo@develer.com>
2016-04-28 17:04:33 +0200

    Refactor objloader: avoid ridiculous duplicated code

--- client/src/loaders/objloader.py
+++ client/src/loaders/objloader.py
@@ -30,66 +30,31 @@ def load_obj(filename):
 
     with open(filename) as f:
         for i, line in enumerate(f):
-            items = line.split()
-            if line.startswith('v '):
-                v0 = float(items[1])
-                v1 = float(items[2])
-                v2 = float(items[3])
-                tmp_vertices.append((v0, v1, v2))
+            header, *data = line.split()
 
-            if line.startswith('vt '):
-                uv0 = float(items[1])
-                uv1 = float(items[2])
-                tmp_uvs.append((uv0, uv1))
+            if header == 'v':
+                tmp_vertices.append([float(x) for x in data])
 
-            if line.startswith('vn '):
-                n0 = float(items[1])
-                n1 = float(items[2])
-                n2 = float(items[3])
-                tmp_normals.append((n0, n1, n2))
+            if header == 'vt':
+                tmp_uvs.append([float(x) for x in data])
 
-            if line.startswith('f '):
-                f0 = items[1]
-                f1 = items[2]
-                f2 = items[3]
+            if header == 'vn':
+                tmp_normals.append([float(x) for x in data])
 
-                f0_items = f0.split('/')
-                idx0 = int(f0_items[0]) - 1
-                indices.append(idx0)
-                try:
-                    idx1 = int(f0_items[1]) - 1
-                except ValueError:
-                    pass
-                else:
-                    uvs += tmp_uvs[idx1]
-                idx2 = int(f0_items[2]) - 1
-                vertices += tmp_vertices[idx0]
-                normals += tmp_normals[idx2]
+            if header == 'f':
 
-                f1_items = f1.split('/')
-                idx0 = int(f1_items[0]) - 1
-                indices.append(idx0)
-                try:
-                    idx1 = int(f1_items[1]) - 1
-                except ValueError:
-                    pass
-                else:
-                    uvs += tmp_uvs[idx1]
-                idx2 = int(f1_items[2]) - 1
-                vertices += tmp_vertices[idx0]
-                normals += tmp_normals[idx2]
-
-                f2_items = f2.split('/')
-                idx0 = int(f2_items[0]) - 1
-                indices.append(idx0)
-                try:
-                    idx1 = int(f2_items[1]) - 1
-                except ValueError:
-                    pass
-                else:
-                    uvs += tmp_uvs[idx1]
-                idx2 = int(f2_items[2]) - 1
-                vertices += tmp_vertices[idx0]
-                normals += tmp_normals[idx2]
+                for face in data:
+                    face_items = face.split('/')
+                    idx0 = int(face_items[0]) - 1
+                    indices.append(idx0)
+                    try:
+                        idx1 = int(face_items[1]) - 1
+                    except ValueError:
+                        pass
+                    else:
+                        uvs += tmp_uvs[idx1]
+                    idx2 = int(face_items[2]) - 1
+                    vertices += tmp_vertices[idx0]
+                    normals += tmp_normals[idx2]
 
     return vertices, normals, uvs, indices
