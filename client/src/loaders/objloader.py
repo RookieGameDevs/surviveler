f203127b1e859293e3b8307c9588259f02a20d61
Iacopo Marmorini <iacopo@develer.com>
2016-04-28 15:34:29 +0200

    Refactor objloader: split line only once

--- client/src/loaders/objloader.py
+++ client/src/loaders/objloader.py
@@ -30,28 +30,25 @@ def load_obj(filename):
 
     with open(filename) as f:
         for i, line in enumerate(f):
+            items = line.split()
             if line.startswith('v '):
-                items = line.split(' ')
                 v0 = float(items[1])
                 v1 = float(items[2])
                 v2 = float(items[3])
                 tmp_vertices.append((v0, v1, v2))
 
             if line.startswith('vt '):
-                items = line.split(' ')
                 uv0 = float(items[1])
                 uv1 = float(items[2])
                 tmp_uvs.append((uv0, uv1))
 
             if line.startswith('vn '):
-                items = line.split(' ')
                 n0 = float(items[1])
                 n1 = float(items[2])
                 n2 = float(items[3])
                 tmp_normals.append((n0, n1, n2))
 
             if line.startswith('f '):
-                items = line.split(' ')
                 f0 = items[1]
                 f1 = items[2]
                 f2 = items[3]
