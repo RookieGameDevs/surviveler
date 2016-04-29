f34897342d88df4008951811b379e46699baf50f
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-04-29 22:02:45 +0200

    Extract os signal handling out of the server code

--- server/src/server/main.go
+++ server/src/server/main.go
@@ -1,10 +1,30 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
 	"server/game"
+	"syscall"
 )
 
 func main() {
+	// game setup
+	cfg := game.GameCfg{
+		Port: "1234",
+	}
 
-	game.StartGameServer()
+	surviveler := new(game.Game)
+	surviveler.Setup(cfg)
+
+	// start the game
+	go surviveler.Start()
+
+	// be notified of termination signals
+	chSig := make(chan os.Signal)
+	signal.Notify(chSig, syscall.SIGINT, syscall.SIGTERM)
+
+	// wait for termination signals
+	fmt.Println("Received signal: ", <-chSig)
+	surviveler.Stop()
 }
