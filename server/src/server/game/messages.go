92bef0e13cfbc245799ba1b13d76b696470af263
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-04-30 10:35:49 +0200

    Create a separate file for msgfactory

--- server/src/server/game/messages.go
+++ server/src/server/game/messages.go
@@ -1,69 +1,25 @@
 package game
 
-import (
-	"fmt"
-	"github.com/ugorji/go/codec"
-	"reflect"
-)
-
-// Types of messages sent by the client
 const (
 	PingId MsgType = 0 + iota
 	PongId
 	PositionId
 )
 
-type MsgFactory struct {
-	registry map[MsgType]reflect.Type
-}
-
-// NewMsgFactory instantiates a new MsgFactory
-func NewMsgFactory() *MsgFactory {
-	mf := new(MsgFactory)
-	mf.registry = make(map[MsgType]reflect.Type)
-	return mf
-}
-
-// RegisterMsgType registers a new MsgType and associates it to a struct type
-func (mf MsgFactory) RegisterMsgType(t MsgType, i interface{}) {
-	// retrieve underlying msg type
-	it := reflect.TypeOf(i)
-	mf.registry[t] = it
-}
-
-func (mf MsgFactory) NewMsg(t MsgType) interface{} {
-	if it, ok := mf.registry[t]; ok {
-		return reflect.New(it).Elem().Interface()
-	}
-	panic(fmt.Sprintf("MsgFactory: MsgType %v not found\n", t))
-}
-
-func (mf MsgFactory) DecodePayload(t MsgType, p []byte) (interface{}, error) {
-	var mh codec.MsgpackHandle
-	// Create a struct having the corresponding underlying type
-	msg := mf.NewMsg(t)
-
-	// Decode msgpack payload into interface
-	var dec *codec.Decoder = codec.NewDecoderBytes(p, &mh)
-	err := dec.Decode(&msg)
-	if err != nil {
-		return nil, fmt.Errorf("Error decoding payload: %v\n", err)
-	}
-
-	return msg, nil
-}
-
-// PositionMsg represents a position in 2D space
-type PositionMsg struct{ Xpos, Ypos float32 }
-
-// Client -> Server time sync message
+/*
+ * Client->server time synchronization message
+ */
 type PingMsg struct {
 	Id     uint32
 	Tstamp int64
 }
 
-// Server -> Client time sync message
-type PongMsg struct {
-	Id     uint32
-	Tstamp int64
-}
+/*
+ * Server->client time synchronization message
+ */
+type PongMsg PingMsg
+
+/*
+ * TEMP: entity position message in 2D space
+ */
+type PositionMsg struct{ Xpos, Ypos float32 }
---
+++ server/src/server/game/msgfactory.go
@@ -0,0 +1,75 @@
+/*
+ * Surviveler Message Factory
+ */
+package game
+
+import (
+	"fmt"
+	"github.com/ugorji/go/codec"
+	"reflect"
+)
+
+/*
+ * MsgFactory associates unique message ids with their corresponding message
+ * structures. Once every know message has been registered with RegisterMsgType,
+ * a new message struct can be instantiated by passing its type to
+ * MsgFactory.NewMsg()
+ */
+type MsgFactory struct {
+	registry map[MsgType]reflect.Type
+}
+
+/*
+ * NewMsgFactory instantiates a new MsgFactory
+ */
+func NewMsgFactory() *MsgFactory {
+	mf := new(MsgFactory)
+	mf.registry = make(map[MsgType]reflect.Type)
+	return mf
+}
+
+/*
+ * Register all message types
+ */
+func (mf MsgFactory) RegisterMsgTypes() {
+	mf.registerMsgType(PingId, PingMsg{})
+	mf.registerMsgType(PongId, PongMsg{})
+	mf.registerMsgType(PositionId, PositionMsg{})
+}
+
+/*
+ * registerMsgType registers a new MsgType and associates it to a struct type
+ */
+func (mf MsgFactory) registerMsgType(t MsgType, i interface{}) {
+	// retrieve underlying msg type
+	it := reflect.TypeOf(i)
+	mf.registry[t] = it
+}
+
+/*
+ * newMsg returns a new (zero'ed) message struct
+ */
+func (mf MsgFactory) newMsg(t MsgType) interface{} {
+	if it, ok := mf.registry[t]; ok {
+		return reflect.New(it).Elem().Interface()
+	}
+	panic(fmt.Sprintf("MsgFactory: MsgType %v not found\n", t))
+}
+
+/*
+ * DecodePayload returns a new message struct, decoded from given payload
+ */
+func (mf MsgFactory) DecodePayload(t MsgType, p []byte) (interface{}, error) {
+	var mh codec.MsgpackHandle
+	// Create a struct having the corresponding underlying type
+	msg := mf.newMsg(t)
+
+	// Decode msgpack payload into interface
+	var dec *codec.Decoder = codec.NewDecoderBytes(p, &mh)
+	err := dec.Decode(&msg)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding payload: %v\n", err)
+	}
+
+	return msg, nil
+}
