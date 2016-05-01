5cef200a8a03a1453b7d729929dbb62b0962843f
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-05-01 22:15:14 +0200

    Implement kickAll, fix in kick

--- server/src/server/game/client.go
+++ server/src/server/game/client.go
@@ -15,7 +15,7 @@ type ClientRegistry struct {
 /*
  * Init initializes the ClientRegistry
  */
-func (reg *ClientRegistry) Init() {
+func (reg *ClientRegistry) init() {
 	reg.clients = make(map[uint16]*network.Conn, 0)
 	reg.nextId = 0
 }
@@ -67,7 +67,7 @@ func (reg *ClientRegistry) getClientId(c *network.Conn) uint16 {
 }
 
 /*
- * kick says out loud "get the fuck out of my server now!"
+ * kick closes a client connection and removes it from the registry
  */
 func (reg *ClientRegistry) kickClient(clientId uint16) {
 	log.WithField("id", clientId).Debug("About to kick client")
@@ -78,11 +78,29 @@ func (reg *ClientRegistry) kickClient(clientId uint16) {
 	if !ok {
 		log.WithField("id", clientId).Panic("Unknown client")
 	}
-	client.Close()
+	delete(reg.clients, clientId)
 	reg.mutex.Unlock()
+
+	client.Close()
 
 	log.WithFields(log.Fields{
 		"id":   clientId,
 		"addr": client.GetRawConn().RemoteAddr(),
 	}).Info("Kicked client")
+}
+
+/*
+ * kickAll says out loud "get the fuck out of my server now!"
+ */
+func (reg *ClientRegistry) kickAll() {
+	log.Debug("About to kick all clients")
+
+	// protect client map access
+	reg.mutex.Lock()
+	for _, client := range reg.clients {
+		client.Close()
+	}
+	reg.mutex.Unlock()
+
+	log.Info("Kicked everybody out")
 }
