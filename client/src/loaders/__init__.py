349ca35a8fc6071623afaa2b900fbcd8fe3b9735
Ivan Nikolaev <voidexp@develer.com>
2016-05-02 18:22:17 +0200

    Style fix

--- client/src/loaders/__init__.py
+++ client/src/loaders/__init__.py
@@ -1,3 +1,3 @@
+from loaders.objloader import OBJFormatError  # noqa
+from loaders.objloader import OBJTypeError  # noqa
 from loaders.objloader import load_obj  # noqa
-from loaders.objloader import OBJFormatError
-from loaders.objloader import OBJTypeError
