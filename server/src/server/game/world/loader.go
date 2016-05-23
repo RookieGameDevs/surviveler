9858d8a33a0ef83c2e89767521c078bbc482e033
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-05-24 00:09:59 +0200

    Load walkables area; grid creation; grid dbg dumps

--- server/src/server/game/world/loader.go
+++ server/src/server/game/world/loader.go
@@ -6,52 +6,86 @@ package world
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"server/game/resource"
 )
 
 type MapData struct {
-	Walkables []resource.Rect2D `json:"walkables"`
-	Objects   []resource.Object `json:"objects"`
+	WorldBoundaries resource.Rect2D   `json:"world_boundaries"`
+	Walkables       []resource.Rect2D `json:"walkables"`
+	Objects         []resource.Object `json:"objects"`
 }
 
 /*
  * LoadWorldFrom initializes a world representation from a SurvivelerPackage
  */
-func LoadWorldFrom(pkg resource.SurvivelerPackage) (w *World, err error) {
-	w = new(World)
+func LoadWorldFrom(pkg resource.SurvivelerPackage) (*World, error) {
 	var data MapData
-	if err = pkg.LoadMap(&data); err != nil {
-		return
+	if err := pkg.LoadMap(&data); err != nil {
+		return nil, err
 	}
 	// semantic validity checks
-	if err = data.IsValid(); err != nil {
-		return
+	if err := data.IsValid(); err != nil {
+		return nil, err
 	}
 
-	// TODO: read map data and fill the World with Tile's
+	w := BrandNewWorld(
+		data.WorldBoundaries[0][0],
+		data.WorldBoundaries[0][1],
+		data.WorldBoundaries[1][0],
+		data.WorldBoundaries[1][1])
 
-	return
+	for i := range data.Walkables {
+		tl_pos := data.Walkables[i][0]
+		br_pos := data.Walkables[i][1]
+		for x := tl_pos[0]; x < br_pos[0]; x++ {
+			for y := tl_pos[1]; y < br_pos[1]; y++ {
+				t := Tile{
+					Kind: KindWalkable,
+					M:    w.Map,
+				}
+				w.SetTile(&t, x, y)
+			}
+		}
+	}
+
+	logw := log.StandardLogger().Writer()
+	defer logw.Close()
+	w.Dump(logw)
+	return w, nil
 }
 
 /*
  * IsValid checks the semantic validity
  */
 func (md MapData) IsValid() error {
+
+	// validate various fields
+	if !md.WorldBoundaries.IsValid() {
+		return fmt.Errorf("Invalid 'world boundaries': %v\n", md.WorldBoundaries)
+	}
 	for i := range md.Walkables {
 		if !md.Walkables[i].IsValid() {
-			return fmt.Errorf("Invalid 'walkables' field: %v\n", md.Walkables[i])
+			return fmt.Errorf("Invalid 'walkable': %v\n", md.Walkables[i])
 		}
 	}
+
 	for i := range md.Objects {
 		if !md.Objects[i].Pos.IsValid() {
-			return fmt.Errorf("Invalid 'objects.pos' field: %v\n", md.Objects[i].Pos)
+			return fmt.Errorf("Invalid 'object' 'pos' field: %v\n", md.Objects[i].Pos)
 		}
 		switch md.Objects[i].Rotation {
 		case 0, 90, 180, 270:
 			break
 		default:
-			return fmt.Errorf("Invalid 'objects.rotation' field: %v\n", md.Objects[i].Rotation)
+			return fmt.Errorf("Invalid 'objects' 'rotation' field: %v\n", md.Objects[i].Rotation)
 		}
 	}
+
+	// actual semantic validation
+
+	// TODO:  check objects are not outside of world boundaries
+	// TODO:  check not objects are inside walkable areas
+
 	return nil
 }
--- server/src/server/game/world/map.go
+++ server/src/server/game/world/map.go
@@ -4,18 +4,97 @@
  */
 package world
 
-import ()
+import (
+	"fmt"
+	"io"
+)
+
+type TileKind int
 
 /*
  * A Tile is a tile in a grid which implements Pather.
  */
 type Tile struct {
-	Kind int   // kind of tile, each kind has its own cost
-	X, Y int   // 2D coordinates of the world
-	W    World // W is a reference to the World that the tile is a part of.
+	Kind TileKind // kind of tile, each kind has its own cost
+	X, Y int      // 2D coordinates
+	M    Map      // reference to the map this is tile is part of
+}
+
+const (
+	KindNotWalkable = iota << 0
+	KindWalkable
+	KindTurret
+)
+
+/*
+ * Map is a two dimensional map of tiles
+ */
+type Map map[int]map[int]*Tile
+
+/*
+ * Tile gets the tile at the given coordinates in the map
+ */
+func (w Map) Tile(x, y int) *Tile {
+	if w[x] == nil {
+		return nil
+	}
+	return w[x][y]
 }
 
 /*
- * World is a two dimensional map of Tiles.
+ * SetTile sets a tile at the given coordinates in the world
  */
-type World map[int]map[int]*Tile
+func (m Map) SetTile(t *Tile, x, y int) {
+	if m[x] == nil {
+		m[x] = map[int]*Tile{}
+	}
+	m[x][y] = t
+	t.X = x
+	t.Y = y
+	t.M = m
+}
+
+/*
+ * World is the spatial reference on which game entities are located
+ */
+type World struct {
+	Map                        // the embedded map
+	Xmin, Xmax, Ymin, Ymax int // world boundaries
+}
+
+/*
+ * BrandNewWorld creates a brand new world
+ */
+func BrandNewWorld(x1, y1 int, x2, y2 int) *World {
+	w := new(World)
+	w.Xmin = x1
+	w.Xmax = x2
+	w.Ymin = y1
+	w.Ymax = y2
+
+	// pre allocate all tiles
+	w.Map = make(map[int]map[int]*Tile)
+	for x := w.Xmin; x < w.Xmax; x++ {
+		w.Map[x] = make(map[int]*Tile)
+		for y := w.Ymin; y < w.Ymax; y++ {
+			w.SetTile(&Tile{
+				Kind: KindNotWalkable,
+				M:    w.Map,
+			}, x, y)
+		}
+	}
+	return w
+}
+
+/*
+ * Dump writes a string representation of the world in the provided Writer
+ */
+func (w World) Dump(w_ io.Writer) {
+	for y := w.Ymin; y < w.Ymax; y++ {
+		for x := w.Xmin; x < w.Xmax; x++ {
+			t := w.Tile(x, y)
+			io.WriteString(w_, fmt.Sprintf("%2v", t.Kind))
+		}
+		io.WriteString(w_, "\n")
+	}
+}
