5acffd0e3f97a73036a382c3192e4aafb4857632
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-06-03 21:50:21 +0200

    Implement flag.Value interface for uint32Value

--- server/src/server/game/utils.go
+++ server/src/server/game/utils.go
@@ -5,6 +5,7 @@
 package game
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -13,4 +14,22 @@ import (
  */
 func MakeTimestamp() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+/*
+ * uint32Value is an explicit 32 bits unsigned type.
+ *
+ * It implements the flag.Value interface
+ */
+type uint32Value uint32
+
+func (i *uint32Value) String() string {
+	return fmt.Sprintf("%v", *i)
+}
+
+func (i *uint32Value) Set(s string) error {
+	if _, err := fmt.Sscanf(s, "%d", i); err != nil {
+		return fmt.Errorf("invalid syntax \"%s\"", s)
+	}
+	return nil
 }
