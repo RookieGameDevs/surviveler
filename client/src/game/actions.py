4d51b59217b87bf620d401d075860084efc6a7d1
Ivan Nikolaev <voidexp@gmail.com>
2016-06-06 19:49:58 +0200

    Trace a ray on mouse click

--- client/src/game/actions.py
+++ client/src/game/actions.py
@@ -1,6 +1,7 @@
 from core.events import MouseClickEvent
 from events import subscriber
 from game.components import Movable
+from matlib import Mat
 from matlib import Vec
 from network import Message
 from network import MessageField
@@ -8,6 +9,36 @@ from network import MessageType
 import logging
 
 LOG = logging.getLogger(__name__)
+
+
+def cast_ray(mouse_coords, view_w, view_h, camera):
+    """Cast a ray from mouse to world."""
+    x, y = mouse_coords
+
+    # transform viewport coordinates to NDC space
+    x_ndc = 2.0 * x / view_w - 1.0
+    y_ndc = 1.0 - 2 * y / view_h
+    z_ndc = -1.0
+
+    # make a homogeneous clip coordinates 4D vector and compute eye coordinates
+    # vector
+    m_projection = Mat(camera.projection)
+    m_projection.invert()
+    v_clip = Vec(x_ndc, y_ndc, z_ndc, 1.0)
+    v_eye = m_projection * v_clip
+
+    # change the vector so that only X and Y components are used and Z just
+    # points forward, W is of no use
+    v_eye.z = -1.0
+    v_eye.w = 0.0
+
+    # transform eye coordinates to world coordinates
+    m_modelview = Mat(camera.modelview)
+    m_modelview.invert()
+    v_world = m_modelview * v_eye
+    v_world.norm()
+
+    return v_world
 
 
 @subscriber(MouseClickEvent)
@@ -19,6 +50,11 @@ def handle_mouse_click(evt):
         renderer_conf = evt.context.conf['Renderer']
         w = int(renderer_conf['width'])
         h = int(renderer_conf['height'])
+
+        ray = cast_ray((evt.x, evt.y), w, h, evt.context.camera)
+        LOG.debug('Ray: {}'.format(ray))
+
+        # TODO: replace this with ray-plane intersection result
 
         # transform viewport coordinates in terms of game units
         fov = evt.context.conf['Game'].getint('fov')
