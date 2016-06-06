3dd8084dbc1c650cd5fe30a77e9f6f46ef892782
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-06-07 00:15:56 +0200

    Replace flag system with cli+ini

--- server/src/server/game/config.go
+++ server/src/server/game/config.go
@@ -5,35 +5,13 @@
 package game
 
 import (
-	"flag"
-	"fmt"
 	log "github.com/Sirupsen/logrus"
-	"github.com/aurelien-rainone/iniflags"
+	"github.com/urfave/cli"
+	"gopkg.in/ini.v2"
+	"os"
 )
 
-var (
-	DefaultPort            = "1234"
-	DefaultLogLevel        = "Debug"
-	DefaultLogicTickPeriod = 10
-	DefaultSendTickPeriod  = 100
-	DefaultTelnetPort      = ""
-	DefaultAssetsPath      = "../data"
-)
-
-var (
-	Port = flag.String("port", DefaultPort,
-		"Server listening port (TCP)")
-	LogLevel = flag.String("log-level", DefaultLogLevel,
-		"Server logging level (Debug, Info, Warning, Error)")
-	LogicTickPeriod = flag.Int("logic-tick-period", DefaultLogicTickPeriod,
-		"Period in millisecond of the ticker that updates game logic")
-	SendTickPeriod = flag.Int("send-tick-period", DefaultSendTickPeriod,
-		"Period in millisecond of the ticker that send the gamestate to clients")
-	TelnetPort = flag.String("telnet-port", DefaultTelnetPort,
-		"Any port different than 0 enables the telnet server (disabled by defaut)")
-	AssetsPath = flag.String("assets", DefaultAssetsPath,
-		"Path to the game assets package")
-)
+const DefaultLogLevel string = "Debug"
 
 /*
  * Config contains all the configurable server-specific game settings
@@ -48,6 +26,36 @@ type Config struct {
 }
 
 /*
+ * fromInifile preloads a cli.Context with values coming from an ini file
+ */
+func fromInifile(c *cli.Context, filename string) error {
+	cfg, err := ini.Load(filename)
+	if err != nil {
+		return err
+	}
+	global := cfg.Section("")
+	if s := global.Key("port").MustString(""); s != "" {
+		c.Set("port", s)
+	}
+	if s := global.Key("log-level").MustString(""); s != "" {
+		c.Set("log-level", s)
+	}
+	if s := global.Key("telnet-port").MustString(""); s != "" {
+		c.Set("telnet-port", s)
+	}
+	if s := global.Key("assets").MustString(""); s != "" {
+		c.Set("assets", s)
+	}
+	if s := global.Key("send-tick-period").MustString(""); s != "" {
+		c.Set("send-tick-period", s)
+	}
+	if s := global.Key("logic-tick-period").MustString(""); s != "" {
+		c.Set("logic-tick-period", s)
+	}
+	return nil
+}
+
+/*
  * ParseConfig parses the configuration settings, fills and returns a Config
  * struct. Settings are read and merged from, (by precedence):
  * - standard Go flags (command line flags)
@@ -55,24 +63,85 @@ type Config struct {
  * - default values
  */
 func ParseConfig() (cfg Config) {
-	iniflags.Parse()
-
-	// fill the Config struct
 	cfg = Config{}
-	cfg.Port = *Port
-	cfg.SendTickPeriod = *SendTickPeriod
-	cfg.LogicTickPeriod = *LogicTickPeriod
-	cfg.TelnetPort = *TelnetPort
-	cfg.AssetsPath = *AssetsPath
 
-	if ll, err := log.ParseLevel(*LogLevel); err == nil {
-		cfg.LogLevel = ll
-	} else {
-		fmt.Printf("Unknown log level: %v, using default %v\n", *LogLevel, DefaultLogLevel)
-		cfg.LogLevel, err = log.ParseLevel(DefaultLogLevel)
-		if err != nil {
-			panic(err)
-		}
+	var logLevel string
+	app := cli.NewApp()
+	app.Metadata = make(map[string]interface{})
+	app.Metadata["config"] = Config{}
+	app.Name = "server"
+	app.Usage = "Surviveler server"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "port",
+			Usage:       "Server listening port (TCP)",
+			Value:       "1234",
+			Destination: &cfg.Port,
+		},
+		cli.StringFlag{
+			Name:        "log-level",
+			Usage:       "Server logging level (Debug, Info, Warning, Error)",
+			Destination: &logLevel,
+		},
+		cli.IntFlag{
+			Name:        "logic-tick-period",
+			Usage:       "Period in millisecond of the ticker that updates game logic",
+			Value:       10,
+			Destination: &cfg.LogicTickPeriod,
+		},
+		cli.IntFlag{
+			Name:        "send-tick-period",
+			Usage:       "Period in millisecond of the ticker that sends the gamestate to clients",
+			Value:       100,
+			Destination: &cfg.SendTickPeriod,
+		},
+		cli.StringFlag{
+			Name:        "telnet-port",
+			Usage:       "Any port different than 0 enables the telnet server (disabled by defaut)",
+			Value:       "",
+			Destination: &cfg.TelnetPort,
+		},
+		cli.StringFlag{
+			Name:        "assets",
+			Usage:       "Path to the game assets package",
+			Value:       "../data",
+			Destination: &cfg.AssetsPath,
+		},
+		cli.StringFlag{
+			Name:  "inifile",
+			Usage: "Path to the server configuration file",
+		},
 	}
+	app.Action = func(c *cli.Context) error {
+		// read config file
+		if inifile := c.String("inifile"); inifile != "" {
+			if err := fromInifile(c, inifile); err != nil {
+				log.WithError(err).Error("Couldn't read config file")
+				return err
+			}
+			log.Warn("other command line flags have no effect in presence of --inifile")
+		}
+
+		// fill the Config struct
+		cfg.Port = c.String("port")
+		cfg.SendTickPeriod = c.Int("send-tick-period")
+		cfg.LogicTickPeriod = c.Int("logic-tick-period")
+		cfg.TelnetPort = c.String("telnet-port")
+		cfg.AssetsPath = c.String("assets")
+		if ll, err := log.ParseLevel(logLevel); err == nil {
+			cfg.LogLevel = ll
+		} else {
+			if logLevel != "" {
+				log.WithFields(log.Fields{"level": logLevel, "default": DefaultLogLevel}).Warn("Unknown log level, using default")
+			}
+			logLevel = DefaultLogLevel
+			cfg.LogLevel, err = log.ParseLevel(logLevel)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+	app.Run(os.Args)
 	return
 }
