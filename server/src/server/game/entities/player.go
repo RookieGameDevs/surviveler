7581ada73afa501d06f2c791f316a3aeb48196ad
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-07-02 20:48:29 +0200

    Player has an ActionStack and uses it

--- server/src/server/game/entities/player.go
+++ server/src/server/game/entities/player.go
@@ -5,10 +5,17 @@
 package entities
 
 import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"server/game"
 	"server/game/components"
 	"server/math"
 	"time"
+)
+
+// player private action types
+const (
+	WaitingForPathAction = 1000 + iota
 )
 
 /*
@@ -21,9 +28,9 @@ const maxWaypointsToSend = 3
  * implements the Entity interface.
  */
 type Player struct {
-	entityType game.EntityType // player type
-	curAction  game.ActionType // current action
 	id         uint32
+	entityType game.EntityType  // player type
+	actions    game.ActionStack // action stack
 	components.Movable
 }
 
@@ -36,7 +43,12 @@ func NewPlayer(id uint32, spawn math.Vec2, speed float64, entityType game.Entity
 	p.entityType = entityType
 	p.Speed = speed
 	p.Pos = spawn
-	p.curAction = game.IdleAction
+
+	// place an idle action as bottommost action stack item this should
+	// never be removed as the player should remain idle when he has nothing
+	// better to do
+	p.actions = *game.NewActionStack()
+	p.actions.Push(&game.Action{game.IdleAction, game.IdleActionData{}})
 	return p
 }
 
@@ -48,18 +60,46 @@ func (p *Player) GetId() uint32 {
  * Update updates the local state of the player
  */
 func (p *Player) Update(dt time.Duration) {
-	if p.curAction == game.MovingAction {
-		p.Movable.Update(dt)
-		if p.Movable.HasReachedDestination() {
-			// come back to Idle if nothing better to do...
-			p.curAction = game.IdleAction
+	// peek the topmost stack action
+	if action, exist := p.actions.Peek(); exist {
+		switch action.Type {
+		case game.MovingAction:
+			// the action data is not interesting as it should always
+			p.Movable.Update(dt)
+			if p.Movable.HasReachedDestination() {
+				// pop current action to get ready for next update
+				p.actions.Pop()
+			}
 		}
+	} else {
+		// little consistency check...
+		log.Panic("There should always be one action in player ActionStack...")
 	}
 }
 
+/*
+ * SetPath defines the path that the player must follow.
+ */
 func (p *Player) SetPath(path math.Path) {
-	p.curAction = game.MovingAction
-	p.Movable.SetPath(path)
+	// consistency check: we should be waiting for a path
+	if action := p.actions.Pop(); action.Type != WaitingForPathAction {
+		log.WithField("action", action.Type).
+			Panic("calling player.SetPath while we are not waiting for a path")
+	} else {
+		p.Movable.SetPath(path)
+	}
+}
+
+/*
+ * Move makes the player move until a specified destination.
+ *
+ * It cancels any high-level actions the player may already be doing and set
+ * the player as moving.
+ */
+func (p *Player) Move(dst math.Vec2) {
+	p.emptyActions()
+	p.actions.Push(&game.Action{game.MovingAction, struct{}{}})
+	p.actions.Push(&game.Action{WaitingForPathAction, struct{}{}})
 }
 
 func (p *Player) GetPosition() math.Vec2 {
@@ -71,29 +111,43 @@ func (p *Player) GetType() game.EntityType {
 }
 
 func (p *Player) GetState() game.EntityState {
-	// first, compile the data depending on current action
-	var actionData interface{}
+	var (
+		actionData interface{}  // action data to be sent
+		curAction  *game.Action // action action from the stack
+	)
 
-	switch p.curAction {
+	curAction, _ = p.actions.Peek()
+	switch curAction.Type {
 	case game.IdleAction:
 		actionData = game.IdleActionData{}
 
 	case game.MovingAction:
-		moveActionData := game.MoveActionData{
+		actionData = game.MoveActionData{
 			Speed: p.Speed,
 			Path:  p.Movable.GetPath(maxWaypointsToSend),
 		}
-		actionData = moveActionData
 	}
+
 	return game.EntityState{
 		Type:       p.entityType,
 		Xpos:       float32(p.Pos[0]),
 		Ypos:       float32(p.Pos[1]),
-		ActionType: p.curAction,
+		ActionType: curAction.Type,
 		Action:     actionData,
 	}
 }
 
 func (p *Player) Build() {
 	fmt.Println("in player.build")
+}
+
+/*
+ * emptyActions removes all the actions from the actions stack.
+ *
+ * It removes all actions but the last one: `IdleAction`.
+ */
+func (p *Player) emptyActions() {
+	// empty the action stack, just let the bottommost (idle)
+	for ; p.actions.Len() > 1; p.actions.Pop() {
+	}
 }
