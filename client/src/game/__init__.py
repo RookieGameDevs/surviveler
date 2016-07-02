d918d529fdcb060351630a2e40e70acca71710ef
Lorenzo Berni <duplo@develer.com>
2016-07-02 12:19:45 +0200

    Split building and building template classes.

--- client/src/game/__init__.py
+++ client/src/game/__init__.py
@@ -1,6 +1,7 @@
 from game.entity import Entity  # noqa
 from game.building import Building  # noqa
 from game.building import BuildingType  # noqa
+from game.building_template import BuildingTemplate  # noqa
 from game.character import Character  # noqa
 from game.character import EntityType  # noqa
 from game.gamestate import process_gamestate  # noqa
