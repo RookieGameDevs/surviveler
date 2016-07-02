61c647bf476aa330de1a0968934ff24b37d9f2ac
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-07-02 20:31:43 +0200

    Remove unused function in math2 package

--- server/src/server/math/vec2.go
+++ server/src/server/math/vec2.go
@@ -33,11 +33,6 @@ func (v Vec2) Elem() (x, y float64) {
 	return v[0], v[1]
 }
 
-// Vec2FromInts creates a Vec2 from a pair of integer numbers
-func Vec2FromInts(x, y int) Vec2 {
-	return Vec2{float64(x), float64(y)}
-}
-
 // Add performs element-wise addition between two vectors. It is equivalent to
 // iterating over every element of v1 and adding the corresponding element of v2
 // to it.
