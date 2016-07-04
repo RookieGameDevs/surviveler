cbb436630cdcb9c204bccdf48bbd691194ee1846
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-07-04 13:53:40 +0200

    BuildingBase Entity interface implementation

--- server/src/server/game/entities/building.go
+++ server/src/server/game/entities/building.go
@@ -5,9 +5,8 @@
 package entities
 
 import (
-	//"server/game"
-	//"server/game/components"
-	//"server/math"
+	"server/game"
+	"server/math"
 	"time"
 )
 
@@ -20,10 +19,34 @@ const (
 
 // BuildingBase is a base containing the required fields for every building
 type BuildingBase struct {
-	totalHP    uint16 // total hit points
-	curHP      uint16 // current hit points
-	requiredBP uint16 // required build power to finish construction
-	curBP      uint16 // current build power induced in the construction
+	totalHP      uint16 // total hit points
+	curHP        uint16 // current hit points
+	requiredBP   uint16 // required build power to finish construction
+	curBP        uint16 // current build power induced in the construction
+	id           uint32
+	pos          math.Vec2
+	buildingType game.EntityType
+}
+
+func (bb *BuildingBase) Type() game.EntityType {
+	return bb.buildingType
+}
+
+func (bb *BuildingBase) Id() uint32 {
+	return bb.id
+}
+
+func (bb *BuildingBase) SetId(id uint32) {
+	bb.id = id
+}
+
+func (bb *BuildingBase) Position() math.Vec2 {
+	return bb.pos
+}
+
+func (bb *BuildingBase) State() game.EntityState {
+	// nothing, builds have no state, at least for now!
+	return game.EntityState{}
 }
 
 type BuildingUpdater interface {
@@ -43,10 +66,11 @@ type BasicBuilding struct {
 func NewBasicBuilding() *BasicBuilding {
 	return &BasicBuilding{
 		BuildingBase{
-			totalHP:    BuildingTotalHitPoints,
-			curHP:      BuildingTotalHitPoints,
-			requiredBP: RequiredBuildPower,
-			curBP:      0,
+			totalHP:      BuildingTotalHitPoints,
+			curHP:        BuildingTotalHitPoints,
+			requiredBP:   RequiredBuildPower,
+			curBP:        0,
+			buildingType: 0,
 		},
 	}
 }
