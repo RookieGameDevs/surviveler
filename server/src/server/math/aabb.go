0c5636c0fab383eba836bfbc59c8c17a004c2082
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-07-10 19:51:08 +0200

    Add utility methods to BoundingBox

--- server/src/server/math/aabb.go
+++ server/src/server/math/aabb.go
@@ -4,6 +4,10 @@ import (
 	gomath "math"
 )
 
+type BoundingBoxer interface {
+	BoundingBox() BoundingBox
+}
+
 // Use NewBoundingBox() to construct a BoundingBox object
 type BoundingBox struct {
 	MinX, MaxX, MinY, MaxY float64
@@ -17,7 +21,22 @@ func NewBoundingBoxFromCircle(center Vec2, radius float64) BoundingBox {
 	x, y := center.Elem()
 	return BoundingBox{x - radius, x + radius, y - radius, y + radius}
 }
+func (b BoundingBox) SizeX() float64 {
+	return b.MaxX - b.MinX
+}
 
-type BoundingBoxer interface {
-	BoundingBox() BoundingBox
+func (b BoundingBox) SizeY() float64 {
+	return b.MaxY - b.MinY
+}
+
+// Returns true if o intersects this
+func (b BoundingBox) Intersects(o BoundingBox) bool {
+	return b.MinX < o.MaxX && b.MinY < o.MaxY &&
+		b.MaxX > o.MinX && b.MaxY > o.MinY
+}
+
+// Returns true if o is within this
+func (b BoundingBox) Contains(o BoundingBox) bool {
+	return b.MinX <= o.MinX && b.MinY <= o.MinY &&
+		b.MaxX >= o.MaxX && b.MaxY >= o.MaxY
 }
