4e3d700f09ae7da8822518f457b662ec0d35c67e
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-07-12 21:14:46 +0200

    Split world.go in 2 files with grid.go

--- server/src/server/game/world.go
+++ server/src/server/game/world.go
@@ -12,8 +12,6 @@ import (
 	"server/math"
 )
 
-type TileList []*Tile
-
 /*
  * World is the spatial reference on which game entities are located
  */
@@ -23,49 +21,6 @@ type World struct {
 	Width, Height         float64             // world dimensions
 	GridScale             float64             // the grid scale
 	Entities              map[uint32]TileList // map entities to the tiles to which it is attached
-}
-
-/*
- * Grid is a two dimensional grid of tiles
- */
-type Grid []Tile
-
-/*
- * A Tile is a tile in a grid which implements Pather.
- *
- * It implements the BoundingBoxer interface and the fmt.GoStringer interface
- * for commodity.
- */
-type Tile struct {
-	Kind     TileKind          // kind of tile, each kind has its own cost
-	X, Y     int               // tile position in 'grid' coordinates
-	W        *World            // reference to the map this tile is part of
-	Entities map[uint32]Entity // Entities intersecting with this Tile
-	aabb     math.BoundingBox  // pre-computed bounding box, as it won't ever change
-}
-
-func NewTile(kind TileKind, w *World, x, y int) Tile {
-	return Tile{
-		Kind:     kind,
-		W:        w,
-		X:        x,
-		Y:        y,
-		Entities: make(map[uint32]Entity),
-		aabb: math.BoundingBox{
-			MinX: float64(x)/w.GridScale - 0.25,
-			MaxX: float64(x)/w.GridScale + 0.25,
-			MinY: float64(y)/w.GridScale - 0.25,
-			MaxY: float64(y)/w.GridScale + 0.25,
-		},
-	}
-}
-
-func (t Tile) GoString() string {
-	return fmt.Sprintf("Tile{X: %d, Y: %d, Kind: %d}", t.X, t.Y, t.Kind)
-}
-
-func (t Tile) BoundingBox() math.BoundingBox {
-	return t.aabb
 }
 
 /*
@@ -102,19 +57,6 @@ func NewWorld(img image.Image, gridScale float64) (*World, error) {
 	}
 	return &w, nil
 }
-
-type TileKind int
-
-// walkability bit
-const (
-	KindNotWalkable TileKind = 0 // non-walkability
-	KindWalkable             = 1 // walkability
-)
-
-// actual tile kinds (bit 0 is set with walkability mask)
-const (
-	KindTurret TileKind = 0x10 | KindNotWalkable
-)
 
 /*
  * Tile gets the tile at the given coordinates in the grid.
