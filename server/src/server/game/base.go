3af4ab2612a9f60f5e3ec9435776081dad7456f8
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-07-29 12:16:01 +0200

    Remove game specific methods from Game interface
    As AiDirector is, by definition, too specific to the game
    implementation, for sure it needs the game implementation and not
    the interface. And it will be the case for any component that
    require the game data.

--- server/src/server/game/base.go
+++ server/src/server/game/base.go
@@ -3,12 +3,13 @@ package game
 import (
 	"server/game/events"
 	"server/game/messages"
-	"server/game/resource"
 	"server/math"
 	"sync"
 )
 
 type EntityFilter func(e Entity) bool
+
+type Data interface{}
 
 type GameState interface {
 	World() *World
@@ -16,14 +17,12 @@ type GameState interface {
 	NearestEntity(pos math.Vec2, f EntityFilter) (Entity, float32)
 	AddEntity(ent Entity)
 	RemoveEntity(id uint32)
-	MapData() *resource.MapData
-	EntityData(EntityType) *resource.EntityData
-	BuildingData(EntityType) *resource.BuildingData
 	GameTime() int16
 }
 
 type Game interface {
 	Start()
+	Data() Data
 	State() GameState
 	QuitChan() chan struct{}
 	MessageChan() chan messages.ClientMessage
