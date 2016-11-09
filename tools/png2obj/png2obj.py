15db78ee7d92dfa482465450a26093ad00eab30a
Iacopo Marmorini <iacopo@develer.com>
2016-11-09 22:26:30 +0100

    edge detector: cleanup old unused code

--- tools/png2obj/png2obj.py
+++ tools/png2obj/png2obj.py
@@ -73,12 +73,6 @@ POSSIBLE_DIRECTIONS = {
 }
 
 
-def load(image_path):
-    image = Image.open(image_path)
-    grayscale = image.convert('L')
-    return np.array(grayscale)
-
-
 class VertexBoxes:
     """Eases the manipulation of a vertex and near squared 2x2 boxes group.
     """
@@ -315,51 +309,8 @@ def mat2map(matrix):
             if not walkable:
                 ret[(x, y)] = 1
     return BlocksMap(ret)
-#assert get_edges(ROW) == [((0, 1), 3)], 'fail: {}'.format(get_edges(ROW))
-
-
-def build_obj(arr, size=1):
-    res = []
-    v_counter = 1
-    vn = 1
-    vertices = []
-    faces = []
-    for y, row in enumerate(arr):
-        for x, value in enumerate(row):
-            if value == 0:
-                v1 = x * size, y * size, 0.0
-                v2 = (x + 1) * size, y * size, 0.0
-                v3 = x * size, (y + 1) * size, 0.0
-                v4 = (x + 1) * size, (y + 1) * size, 0.0
-                vertices.append('v {:.6f} {:.6f} {:.6f}'.format(*v1))
-                vertices.append('v {:.6f} {:.6f} {:.6f}'.format(*v2))
-                vertices.append('v {:.6f} {:.6f} {:.6f}'.format(*v3))
-                vertices.append('v {:.6f} {:.6f} {:.6f}'.format(*v4))
-                face = 'f {v1_i} {v2_i} {v3_i} {v4_i}'.format(
-                    v1_i=v_counter, v2_i=v_counter + 1, v3_i=v_counter + 3, v4_i=v_counter + 2
-                )
-                faces.append(face)
-                v_counter += 4
-                vn += 1
-    res.extend(vertices)
-    res.append('s off')
-    res.extend(faces)
-    return '\n'.join(res)
-
-
-def export_obj(obj, dst):
-    with open(dst, 'w') as fp:
-        fp.write(obj)
-    return os.path.getsize(dst)
-
-
-def main(image_filepath):
-    arr = load(image_filepath)
-    obj = build_obj(arr)
-    print(export_obj(obj, 'test.obj'))
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
     import doctest
     doctest.testmod()
