f1079e5c56dca9c12d4c133cb8c194efa2c71312
Iacopo Marmorini <iacopo@develer.com>
2016-11-09 21:15:50 +0100

    wip edge finder (pass 4/15 tests); do not remove internal edge vertices

--- tools/png2obj/png2obj.py
+++ tools/png2obj/png2obj.py
@@ -28,61 +28,47 @@ ROW = np.array(
     ])
 
 
-LEFT = np.array([-1, 0])
-UP = np.array([0, 1])
-RIGHT = np.array([1, 0])
-DOWN = np.array([0, -1])
+LEFT = (-1, 0)
+UP = (0, -1)
+RIGHT = (1, 0)
+DOWN = (0, 1)
+HERE = (0, 0)
+VERSOR_NAME = {LEFT: 'left', UP: 'up', RIGHT: 'right', DOWN: 'down', HERE: 'here'}
 
 
-DIRECTIONS = {
-    (
-        (0, 0),
-        (0, 0)): (),
-    (
-        (0, 0),
-        (0, 1)): (RIGHT, DOWN),
-    (
-        (0, 0),
-        (1, 0)): (LEFT, DOWN),
-    (
-        (0, 0),
-        (1, 1)): (LEFT, RIGHT),
-    (
-        (0, 1),
-        (0, 0)): (UP, RIGHT),
-    (
-        (0, 1),
-        (0, 1)): (UP, DOWN),
-    (
-        (0, 1),
-        (1, 0)): (LEFT, UP, RIGHT, DOWN),
-    (
-        (0, 1),
-        (1, 1)): (LEFT, UP),
-    (
-        (1, 0),
-        (0, 0)): (LEFT, UP),
-    (
-        (1, 0),
-        (0, 1)): (LEFT, UP, RIGHT, DOWN),
-    (
-        (1, 0),
-        (1, 0)): (UP, DOWN),
-    (
-        (1, 0),
-        (1, 1)): (UP, RIGHT),
-    (
-        (1, 1),
-        (0, 0)): (LEFT, RIGHT),
-    (
-        (1, 1),
-        (0, 1)): (LEFT, DOWN),
-    (
-        (1, 1),
-        (1, 0)): (RIGHT, DOWN),
-    (
-        (1, 1),
-        (1, 1)): (),
+POSSIBLE_DIRECTIONS = {
+    ((0, 0),
+     (0, 0)): (),
+    ((0, 0),
+     (0, 1)): (RIGHT, DOWN),
+    ((0, 0),
+     (1, 0)): (LEFT, DOWN),
+    ((0, 0),
+     (1, 1)): (LEFT, RIGHT),
+    ((0, 1),
+     (0, 0)): (UP, RIGHT),
+    ((0, 1),
+     (0, 1)): (UP, DOWN),
+    ((0, 1),
+     (1, 0)): (LEFT, UP, RIGHT, DOWN),
+    ((0, 1),
+     (1, 1)): (LEFT, UP),
+    ((1, 0),
+     (0, 0)): (LEFT, UP),
+    ((1, 0),
+     (0, 1)): (LEFT, UP, RIGHT, DOWN),
+    ((1, 0),
+     (1, 0)): (UP, DOWN),
+    ((1, 0),
+     (1, 1)): (UP, RIGHT),
+    ((1, 1),
+     (0, 0)): (LEFT, RIGHT),
+    ((1, 1),
+     (0, 1)): (LEFT, DOWN),
+    ((1, 1),
+     (1, 0)): (RIGHT, DOWN),
+    ((1, 1),
+     (1, 1)): (),
 }
 
 
@@ -110,10 +96,13 @@ class VertexBoxes:
         self.left_boxes = self.boxes.upleft, self.boxes.downleft
 
         # 2 x 2 matrix cotaining box values relative to the vertex
-        self.block_matrix = [
-            [self.map.get(self.boxes.upleft, 0), self.map.get(self.boxes.upright, 0)],
-            [self.map.get(self.boxes.downleft, 0), self.map.get(self.boxes.downright, 0)],
-        ]
+        self.block_matrix = (
+            (self.map.get(self.boxes.upleft, 0), self.map.get(self.boxes.upright, 0)),
+            (self.map.get(self.boxes.downleft, 0), self.map.get(self.boxes.downright, 0)),
+        )
+
+    def __repr__(self):
+        return repr(self.block_matrix)
 
 
 def is_black(arr, xy):
@@ -212,37 +201,95 @@ class Map(dict):
         """
         ret = []
         v_boxes = self.vertex2boxes(vertex)
-        versors = DIRECTIONS[v_boxes.block_matrix]
-        ret = [vertex + v for v in versors]
-        print('next({}) = {}'.format(vertex, ret))
+        #print(v_boxes)
+        versors = POSSIBLE_DIRECTIONS[v_boxes.block_matrix]
+        #print('versors =', versors)
+        ret = [tuple(vertex + np.array(v)) for v in versors]
+        #print('next({}) = {}'.format(vertex, ret))
         return ret
 
     def build(self):
+        ret = []
         #start_box = find_box(self.arr)
+        if not self.map:
+            return []
+
         start_box = min(sorted(self.map.keys()))
 
         v0 = self.map_box(start_box)
-        print('Start from {}'.format(v0))
+        #print('Start from {}'.format(v0))
         #self.vertices[v0] = None
 
-        vertex = v0
+        vertex = first_vertex = v0
         tracked = [v0]
+        old_versor = (0, 0)  # like a `None` but supporting the array sum
+        ret.append(v0)
+        closable = False
         while True:
-            for v_next in self.get_next_block_vertices(vertex):
+            print('Vertex: {}'.format(vertex))
+            vertices = self.get_next_block_vertices(vertex)
+            #print('Next vertices =', vertices)
+            # Cycle through new possible vertices to explore
+            for v_next in vertices:
+                versor_next = tuple(np.array(v_next) - tracked[-1])
+                print('Exploring {} -> {}'.format(VERSOR_NAME[versor_next], v_next))
+
+                # Avoid to go back
+                if not(np.array(old_versor) + np.array(versor_next)).any():
+                    print('Do not go just back to {}'.format(v_next))
+                    continue
+
                 if v_next not in tracked:
+                    print('Found new vertex to go: {}'.format(v_next))
+                    ret.append(v_next)
                     break
                 else:
+                    if v_next == ret[0]:
+                        # could close the polygon
+                        closable = True
                     print('{} in tracked {}'.format(v_next, tracked))
 
             if v_next in tracked:
+                if closable:
+                    print('Closing the polygon')
+                    ret.append(first_vertex)
                 break
-            print(v_next)
+
+            #print(v_next)
+            versor = tuple(np.array(v_next) - tracked[-1])
+            versor_name = VERSOR_NAME[versor]
+            #print('versor =', versor)
             tracked.append(v_next)
+            if versor != old_versor:
+                print('changed versor')
+                #ret.append(vertex)
+            else:
+                print('same versor')
+            print('going {}'.format(versor_name))
+
             vertex = v_next
+            old_versor = tuple(versor)
 
+            print(ret)
         # find new contiguous free position to move vertex to
+        print(tracked)
+        return [ret]
 
 
+map_sample = BlocksMap({
+    (1, 1): 1, (2, 1): 1,
+})
+
+
+def mat2map(matrix):
+    """Create a block map.
+    """
+    ret = {}
+    for y, row in enumerate(matrix):
+        for x, walkable in enumerate(row):
+            if not walkable:
+                ret[(x, y)] = 1
+    return BlocksMap(ret)
 #assert get_edges(ROW) == [((0, 1), 3)], 'fail: {}'.format(get_edges(ROW))
 
 
