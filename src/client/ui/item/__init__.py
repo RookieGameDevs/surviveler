6877f0b54adec69f0e11a7427d6312bed46055e0
Lorenzo Berni <duplo@develer.com>
2016-11-15 19:50:30 +0100

    Rework the item class.

--- src/client/ui/item/__init__.py
+++ src/client/ui/item/__init__.py
@@ -10,6 +10,145 @@ class ValidationError(Exception):
     """Exception class for validation errors"""
 
 
+def validate_item(source_data):
+    """Check for missing positioning information.
+
+    This method validates the positioning information of the item.
+
+    For an item to be valid the following conditions need to be true:
+        * available x coordinate (directly or via anchor)
+        * available y coordinate (directly or via anchor)
+        * width available (directly or via anchor)
+        * height available (directly or via anchor)
+
+    This method just does static validation: eventual conflicts due to
+    circular dependencies will be validated at binding time.
+
+    In case of validation error a ValidationError is raised.
+
+    :param source_data: The original source data
+    :type source_data: :class:`dict`
+
+    :returns: The validated source data
+    :rtype: :class:`dict`
+    """
+    AT = Anchor.AnchorType
+    sd = source_data
+
+    def get(anchor, at):
+        if anchor:
+            return anchor.get(at)
+
+    # For the x coordinate to be considered defined the item needs to:
+    # 1. have a defined position
+    # 2. have the left anchor defined
+    # 3. have the horizontal center and the width defined
+    # 4. have the right anchor and the width defined
+    x_coord_available = any([
+        sd['position'] is not None,
+        get(sd['anchor'], AT.left) is not None,
+        get(sd['anchor'], AT.hcenter) is not None and sd['width'] is not None,
+        get(sd['anchor'], AT.right) is not None and sd['width']
+    ])
+
+    # For the y coordinate to be considered defined the item needs to:
+    # 1. have a defined position
+    # 2. have the top anchor defined
+    # 3. have the vertical center and the height defined
+    # 4. have the bottom anchor and the height defined
+    y_coord_available = any([
+        sd['position'] is not None,
+        get(sd['anchor'], AT.top) is not None,
+        get(sd['anchor'], AT.vcenter) is not None and sd['height'] is not None,
+        get(sd['anchor'], AT.bottom) is not None and sd['height']
+    ])
+
+    # For the width to be considered defined the item needs to:
+    # 1. have a defined width
+    # 2. have at least two horizontal anchors defined
+    width_available = any([
+        sd['width'] is not None,
+        get(sd['anchor'], AT.left) is not None and get(sd['anchor'], AT.right) is not None,
+        get(sd['anchor'], AT.left) is not None and get(sd['anchor'], AT.hcenter) is not None,
+        get(sd['anchor'], AT.hcenter) is not None and get(sd['anchor'], AT.right) is not None
+    ])
+
+    # For the height to be considered defined the item needs to:
+    # 1. have a defined height
+    # 2. have at least two vertical anchors defined
+    height_available = any([
+        sd['height'] is not None,
+        get(sd['anchor'], AT.top) is not None and get(sd['anchor'], AT.bottom) is not None,
+        get(sd['anchor'], AT.top) is not None and get(sd['anchor'], AT.vcenter) is not None,
+        get(sd['anchor'], AT.vcenter) is not None and get(sd['anchor'], AT.bottom) is not None
+    ])
+
+    if not all([x_coord_available, y_coord_available, width_available, height_available]):
+        raise ValidationError('Not enough information for positioning and layouting')
+
+    return sd
+
+
+def calculate_dimension(anchor, *anchors):
+    """Calculate height or width, based on the relevant anchor values.
+
+    As relevant anchor values we consider both:
+        * [left, hcenter, right] -> width
+        * [top, vcenter, bottom] -> height
+
+    :param anchor: The anchor values
+    :type anchor: :class:`dict`
+
+    :param *anchors: List of relevant anchor values
+    :type *anchors: :class:`list`
+
+    :returns: The dimension
+    :rtype: :class:`int`
+    """
+    first, second, third = anchors
+
+    dimension = 0
+    if first in anchor and second in anchor:
+        dimension = (anchor[second] - anchor[first]) * 2
+    elif first in anchor and third in anchor:
+        dimension = anchor[third] - anchor[first]
+    elif second in anchor and third in anchor:
+        dimension = (anchor[third] - anchor[second]) * 2
+    return dimension
+
+
+def calculate_coord(dimension, anchor, *anchors):
+    """Calculate coordinate using the relevant size and anchor values.ABCMeta
+
+    As relevant anchor values we consider both:
+        * width, [left, hcenter, right] -> x
+        * height, [top, vcenter, bottom] -> y
+
+    :param dimension: The relevant dimension
+    :type dimension: :class:`int`
+
+    :param anchor: The anchor values
+    :type anchor: :class:`dict`
+
+    :param *anchors: List of relevant anchor values
+    :type *anchors: :class:`list`
+
+    :returns: The coordinate value
+    :rtype: :class:`int`
+    """
+    first, second, third = anchors
+
+    coord = 0
+    if first in anchor:
+        coord = anchor[first]
+    elif second in anchor:
+        coord = anchor[second] - int(dimension / 2)
+    elif third in anchor:
+        coord = anchor[third] - dimension
+
+    return coord
+
+
 class Item(metaclass=ABCMeta):
     """The basic item class.
 
@@ -41,67 +180,29 @@ class Item(metaclass=ABCMeta):
         self._margin = {}
 
         # Source geometry properties
-        self._source_data = {
+        self._source_data = validate_item({
             'position': kwargs.get('position'),
             'width': kwargs.get('width'),
             'height': kwargs.get('height'),
             'anchor': kwargs.get('anchor'),
             'margin': kwargs.get('margin'),
-        }
-
-        self.validate_item()
-
-    def validate_item(self):
-        """Check for missing positioning information.
-        """
-        # TODO: add documentation
-
-        AT = Anchor.AnchorType
-        sd = self._source_data
-
-        def get(anchor, at):
-            if anchor:
-                return anchor.get(at)
-
-        x_coord_available = any([
-            sd['position'] is not None,
-            get(sd['anchor'], AT.left) is not None,
-            get(sd['anchor'], AT.hcenter) is not None and sd['width'] is not None,
-            get(sd['anchor'], AT.right) is not None and sd['width']
-        ])
-
-        y_coord_available = any([
-            sd['position'] is not None,
-            get(sd['anchor'], AT.top) is not None,
-            get(sd['anchor'], AT.vcenter) is not None and sd['height'] is not None,
-            get(sd['anchor'], AT.bottom) is not None and sd['height']
-        ])
-
-        width_available = any([
-            sd['width'] is not None,
-            get(sd['anchor'], AT.left) is not None and get(sd['anchor'], AT.right) is not None,
-            get(sd['anchor'], AT.left) is not None and get(sd['anchor'], AT.hcenter) is not None,
-            get(sd['anchor'], AT.hcenter) is not None and get(sd['anchor'], AT.right) is not None
-        ])
-
-        height_available = any([
-            sd['height'] is not None,
-            get(sd['anchor'], AT.top) is not None and get(sd['anchor'], AT.bottom) is not None,
-            get(sd['anchor'], AT.top) is not None and get(sd['anchor'], AT.vcenter) is not None,
-            get(sd['anchor'], AT.vcenter) is not None and get(sd['anchor'], AT.bottom) is not None
-        ])
-
-        if not all([x_coord_available, y_coord_available, width_available, height_available]):
-            raise ValidationError('Not enough information for positioning and layouting')
+        })
 
     def bind_item(self):
         """Bind the item and trigger the binding of the child items.
+
+        The binding process is made of the following steps:
+            1. Calculating the available anchor values (based on the relations).
+            2. Calculating width and height (if not available).
+            3. Calculation the position (if not available).
+            4. Cache all the anchor values.
+            5. Call the binding on all the children items.
         """
         sd = self._source_data
 
         # Calculate the values of the configured anchors
         if sd['anchor'] is not None:
-            self.calculate_anchor()
+            self._anchor = self.calculate_anchor()
 
         # Calculate the width
         if sd['width'] is not None:
@@ -124,71 +225,67 @@ class Item(metaclass=ABCMeta):
         # Recalculate all the anchors with the internal data for caching purpose
         self._anchor = self.cache_anchor()
 
+        # Call the bind method on each children item
         for ref, item in self.children.items():
             item.bind_item()
 
     def calculate_anchor(self):
-        """TODO: add documentation
+        """Calculate the anchor values.
+
+        Uses the source anchor to calculate the anchor values for each available
+        reference.
+
+        :returns: The anchor values
+        :rtype: :class:`dict`
         """
         # Mapping of targets with real items
-        self._anchor = {}
+        anchor = {}
         for t, (target, tt) in self._source_data['anchor'].items():
             item = self.parent
             if target != 'parent':
                 item = self.parent.get_child(target)
-            self._anchor[t] = item.anchor[tt]
-
-    def _calculate_dimension(self, *anchors):
-        """TODO: add documentation
-        """
-        first, second, third = anchors
-
-        dimension = 0
-        if first in self._anchor and second in self._anchor:
-            dimension = (self._anchor[second] - self._anchor[first]) * 2
-        elif first in self._anchor and third in self._anchor:
-            dimension = self._anchor[third] - self._anchor[first]
-        elif second in self._anchor and third in self._anchor:
-            dimension = (self._anchor[third] - self._anchor[second]) * 2
-        return dimension
+            anchor[t] = item.anchor[tt]
+        return anchor
 
     def calculate_width(self):
-        """TODO: add documentation
+        """Calculate the width using the anchor values.
+
+        :returns: The width
+        :rtype: :class:`int`
         """
         AT = Anchor.AnchorType
-        return self._calculate_dimension(AT.left, AT.hcenter, AT.right)
+        return calculate_dimension(self._anchor, AT.left, AT.hcenter, AT.right)
 
     def calculate_height(self):
-        """TODO: add documentation
+        """Calculate the height using the anchor values.
+
+        :returns: The height
+        :rtype: :class:`int`
         """
         AT = Anchor.AnchorType
-        return self._calculate_dimension(AT.top, AT.vcenter, AT.bottom)
-
-    def _calculate_coord(self, dimension, *anchors):
-        """TODO: add documentation
-        """
-        first, second, third = anchors
-
-        coord = 0
-        if first in self._anchor:
-            coord = self._anchor[first]
-        elif second in self._anchor:
-            coord = self._anchor[second] - int(dimension / 2)
-        elif third in self._anchor:
-            coord = self._anchor[third] - dimension
-
-        return coord
+        return calculate_dimension(self._anchor, AT.top, AT.vcenter, AT.bottom)
 
     def calculate_position(self):
-        """TODO: add documentation
+        """Calculate the position using the available information.
+
+        :returns: The position
+        :rtype: :class:`..Point`
         """
         AT = Anchor.AnchorType
 
         return Point(
-            self._calculate_coord(self._width, AT.left, AT.hcenter, AT.right),
-            self._calculate_coord(self._height, AT.top, AT.vcenter, AT.bottom))
+            calculate_coord(self._width, self._anchor, AT.left, AT.hcenter, AT.right),
+            calculate_coord(self._height, self._anchor, AT.top, AT.vcenter, AT.bottom))
 
     def cache_anchor(self):
+        """Cache all the anchor values.
+
+        The cached anchor values are not going to be modified until a new bind
+        operation is called.
+
+        :returns: The anchor values
+        :rtype: :class:`dict`
+        """
         AT = Anchor.AnchorType
         x = self._position.x
         y = self._position.y
@@ -203,42 +300,57 @@ class Item(metaclass=ABCMeta):
 
     @property
     def position(self):
-        """TODO: add documentation
+        """Readonly top-left position of the item.
+
+        :returns: The position of the Item
+        :rtype: :class:`..Point`
         """
         return self._position
 
     @property
     def width(self):
-        """TODO: add documentation
+        """Readonly width of the item.
+
+        :returns: The width of the item
+        :rtype: :class:`..Point`
         """
         return self._width
 
     @property
     def height(self):
-        """TODO: add documentation
+        """Readonly height of the item.
+
+        :returns: The height of the item
+        :rtype: :class:`..Point`
         """
         return self._height
 
     @property
     def anchor(self):
-        """TODO: add documentation
+        """Readonly anchor values of the item.
+
+        :returns: The anchor values
+        :rtype: :class:`dict`
         """
         return self._anchor
 
     @property
     def margin(self):
-        """TODO: add documentation
+        """Readonly margin values of the item
+
+        :returns: The margin values
+        :rtype: :class:`dict`
         """
         return self._margin
 
     def add_child(self, ref, item):
         """Attaches a child to the item, and binds the properties.
 
-        : param ref: The name that identifies internally the child
-        : type ref:: class: `str`
+        :param ref: The name that identifies internally the child
+        :type ref: :class:`str`
 
-        : param item: The actual item to be added as child
-        : type item:: class: `ui.item.Item`
+        :param item: The actual item to be added as child
+        :type item: :class:`ui.item.Item`
         """
         self.children[ref] = item
 
@@ -249,7 +361,7 @@ class Item(metaclass=ABCMeta):
         This method should be implemented by subclasses of Item. It describes
         the behavior the class should have during updates.
 
-        : param dt: The time delta since last update(in seconds)
-        : type dt:: class: `float`
+        :param dt: The time delta since last update(in seconds)
+        :type dt: :class:`float`
         """
         pass
