9d395b479c7a148e88a99a84d63db5814a2c7ba1
Lorenzo Berni <duplo@develer.com>
2016-12-29 19:48:00 +0100

    Add documentation

--- src/client/ui/__init__.py
+++ src/client/ui/__init__.py
@@ -5,20 +5,48 @@ from enum import Enum
 
 
 class EventType(Enum):
+    """Enum of the various event types handled by the User Interface.
+    """
     mouse_click = 'mouse_click'
     mouse_move = 'mouse_move'
     key_press = 'key_press'
 
 
 class UI:
+    """User interface class.
+    """
     def __init__(self, width, height):
+        """Constructor.
+
+        :param width: The width of the UI
+        :type width: :class:`int`
+
+        :param height: The height of the UI
+        :type height: :class:`int`
+        """
         self._root = RootItem(width, height)
 
     @property
     def root(self):
+        """Property containing the root item of the UI.
+        """
         return self._root
 
     def dispatch(self, event_type, pos=None, payload=None):
+        """Traverses the item tree and dispatches the event properly.
+
+        :param event_type: The event type
+        :type event_type: :class:`EventType`
+
+        :param pos: The position of the event (optional)
+        :type pos: :class:`.util.point.Point`
+
+        :param payload: The payload of the event
+        :type payload: :class:`dict`
+
+        :returns: True if the event was handled, False otherwise.
+        :rtype: :class:`bool`
+        """
         stop_propagation = False
         for item in self._root.traverse(listen_to=event_type, pos=pos):
             stop_propagation = item.handle(event_type, payload)
@@ -27,6 +55,17 @@ class UI:
         return stop_propagation
 
     def mouse_click_event_handler(self):
+        """Returns the mouse click event handler function.
+
+        To dispatch properly the events to the user interface, the caller needs
+        to get a event handler and call it with the appropriate arguments.
+
+        The mouse click event handler accepts three arguments: the button, the
+        state and the pos.
+
+        :returns: The mouse click event handler
+        :rtype: :class:`function`
+        """
         ET = EventType
         return (
             lambda button, state, pos:
@@ -37,15 +76,37 @@ class UI:
         )
 
     def mouse_move_event_handler(self):
+        """Returns the mouse move event handler function.
+
+        To dispatch properly the events to the user interface, the caller needs
+        to get a event handler and call it with the appropriate arguments.
+
+        The mouse move event handler accepts only one argument: the cursor
+        position.
+
+        :returns: The mouse move event handler
+        :rtype: :class:`function`
+        """
         ET = EventType
         return (
             lambda pos:
-            self.dispatch(ET.mouse_click, pos=pos)
+            self.dispatch(ET.mouse_move, pos=pos)
         )
 
     def key_press_event_handler(self):
+        """Returns the key press event handler function.
+
+        To dispatch properly the events to the user interface, the caller needs
+        to get a event handler and call it with the appropriate arguments.
+
+        The key press event handler accepts two arguments: the pressed key and
+        the state.
+
+        :returns: The mouse move event handler
+        :rtype: :class:`function`
+        """
         ET = EventType
         return (
             lambda key, state:
-            self.dispatch(ET.mouse_click, key=key, payload={'state': state})
+            self.dispatch(ET.key_press, key=key, payload={'state': state})
         )
